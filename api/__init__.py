e7de742ae8a76cb393af7d25f7d6f7b0a181041c
ronnyno88 <95188001+ronnyno88@users.noreply.github.com>
2023-10-09 20:01:41 -0300

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-from flask_jwt_extended import JWTManager
+
 
 import config
 
